6fb7bbcaa868dcdd091c8e4ab775544cfeee6d8c
mahalaxmi <mvtarapur@gmail.com>
2022-04-07 20:14:41 +0530

    added covid api

--- src/main/java/com/bytestream/controller/CovidController.java
+++ src/main/java/com/bytestream/controller/CovidController.java
@@ -1,22 +1,36 @@
 package com.bytestream.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bytestream.entity.CovidData;
 
 @RestController
 @RequestMapping("/api/v1")
 public class CovidController 
 {
-	@GetMapping("/mark")
-	public String getHelloWorld()
+	@CrossOrigin("*")
+	@GetMapping("/getCovidData")
+	public List<CovidData> getCovidData()
+	
 	{
-		return "Hello world!";
-	}
-
-	@GetMapping("/goodbye")
-	public String getGoodBye()
-	{
-		return "Good Bye Spring Boot Application!";
+		CovidData data  = new CovidData(100, 200, "New York", 400);
+		CovidData data1  = new CovidData(200, 300, "London", 500);
+		CovidData data2  = new CovidData(300, 400, "Paris", 400);
+		CovidData data3  = new CovidData(400, 500, "Rome", 300);
+		
+		List<CovidData> list = new ArrayList();
+		list.add(data);
+		list.add(data1);
+		list.add(data2);
+		list.add(data3);
+		
+		return list;
+		
 	}
 }
